17b8a76935b09c5ed5c3c4b350b0e9c10559779e
tung_le_thanh <tung_le_thanh@bita.jp>
2023-07-28 13:17:00 +0700

    fix: cors policy

--- service_first/src/app.js
+++ service_first/src/app.js
@@ -7,6 +7,7 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 // Database
 const mongoose = require("mongoose");
