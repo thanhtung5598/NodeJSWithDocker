0d54e6843495ff4b0ab91d6d67aaf88db40f3488
tung_le_thanh <tung_le_thanh@bita.jp>
2023-08-01 10:55:20 +0700

    fix: cors

--- auth_service/src/app.js
+++ auth_service/src/app.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 
-const cors = require("cors");
 const express = require("express");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
@@ -17,13 +16,6 @@ app.use(
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next()
-})
 
 // Database
 const mongoose = require("mongoose");
