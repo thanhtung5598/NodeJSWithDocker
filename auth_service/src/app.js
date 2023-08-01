419a4149dba6201d64cb31aa7e62224f63316358
tung_le_thanh <tung_le_thanh@bita.jp>
2023-08-01 11:08:01 +0700

    fix: added cors policy

--- auth_service/src/app.js
+++ auth_service/src/app.js
@@ -18,7 +18,12 @@ app.use(
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 // Database
 const mongoose = require("mongoose");
 mongoose
--- service_second/src/app.js
+++ service_second/src/app.js
@@ -9,7 +9,12 @@ const port = process.env.PORT;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 // Database
 const mongoose = require("mongoose");
 mongoose
